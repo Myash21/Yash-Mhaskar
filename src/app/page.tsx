1c3d74c88e4c86e9c0331a6b8a99ead785504b92
Yash Mhaskar <myash100@gmail.com>
2024-09-15 10:14:25 -0700

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,7 +126,7 @@ export default function Page() {
                   dates={project.dates}
                   tags={project.technologies}
                   image={project.image}
-                  video={project.video}
+                  //video={project.video}
                   links={project.links}
                 />
               </BlurFade>
